0b0cdc89fe983ccf17ed20c5adfc296a3284b3da
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-10-06 22:14:42 +0300

    corrected PEP8validation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,9 +7,10 @@
  if n <= 0 return  an empty list
  Assume n will always be an integer
 
- 
+
 
 """
+
 
 def pascal_triangle(n):
     """
@@ -23,7 +24,7 @@ def pascal_triangle(n):
 
         triangle = []
         for i in range(n):
-            row = [1] * (i + 1) 
+            row = [1] * (i + 1)
             for j in range(1, i):
                 row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
             triangle.append(row)
